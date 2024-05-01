90725a3f376d9923ed73a92e3538a854edfdbd9d
Lucas Bitencourt <lblukz@gmail.com>
2024-05-01 19:48:41 -0300

    Atualiza estilos e conteúdo da página de produto

--- produto.js
+++ produto.js
@@ -14,12 +14,14 @@ const renderProduct = async () => {
   const productElement = document.createElement("div");
   productElement.className = "produto";
   productElement.innerHTML = `
-    <h3 class="titulo">${product.title}</h3>
-    <img class="imagem" src="${product.thumbnail}" alt="${product.title}">
+    <h1 class="titulo">${product.title}</h1>
+    <img class="imagem" src="${product.pictures[0].url}" alt="${product.title}">
     <p class="preco">Preço: <span>${product.price.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL",
     })}</span></p>
+    <p>Quantidade disponível: ${product.initial_quantity} unidades!</p>
+    <p>Data de criação: ${new Date(product.date_created).toLocaleDateString()}</p>
     <p class="frete">${product.shipping.free_shipping ? "Frete grátis!" : ""}</p>
     <button class="comprar">Comprar</button>
   `;
